10c7fc8a7959a19889f7531b9626f5d28a4f9d99
Girts Strazdins <girts.strazdins@gmail.com>
2022-09-07 20:22:07 +0200

    Update  protocol description

--- src/main/java/no/ntnu/datakomm/Encoder.java
+++ src/main/java/no/ntnu/datakomm/Encoder.java
@@ -9,15 +9,23 @@ public class Encoder {
      * The protocol for encoding
      * ------------------------------------------------------------
      * Human-readable message strings are encoded to bit-strings according to the following rules:
-     * 1) Each character of the message is translated to an integer code first, by taking the ASCII code of that
+     * 1) Only the following characters are allowed in the plain-text, human-readable messages:
+     *   - lowercase letters a-z
+     *   - uppercase letters A-Z
+     *   - digits 0-9
+     *   - space ' '
+     *   - comma ','
+     *   - period '.'
+     *   - exclamation mark '!'
+     * 2) Each character of the message is translated to an integer code first, by taking the ASCII code of that
      *    character. See ASCII table here: https://www.asciitable.com/
      *    Examples: uppercase letter `A` has ASCII code 65, letter `B` has ASCII code 66, etc.; while lowercase
      *    letter `a` has code 97, lowercase `b` has code 98, etc.
-     * 2) Then the integer code is translated to a binary string, always eight bits for each integer code.
+     * 3) Then the integer code is translated to a binary string, always eight bits for each integer code.
      *    The binary string contains the most significant bit first.
      *    Examples: number 65 is translated to "01000001", 66 is translated to "01000010", 98 is translated to
      *    "01100010". Number 0 is translated to "00000000" and number 255 is translated to "11111111".
-     * 3) The binary strings for each character are then concatenated in the order as they appear.
+     * 4) The binary strings for each character are then concatenated in the order as they appear.
      *    Examples:
      *      "ABC" -> 65, 66, 67 -> "010000010100001001000011"
      *      "Hi!" -> 72, 105, 33 -> "010010000110100100100001"
@@ -26,7 +34,9 @@ public class Encoder {
      * 1) Split the bit string into 8-character chunks (8-bit blocks)
      * 2) Convert each 8-bit block to an integer
      * 3) Convert the integer to a character, using the ASCII table
-     * 4) Concatenate the individual characters together to form a message
+     * 4) Only the same limited set of characters is allowed (a-z, A-Z, 0-9, space, comma, period, exclamation mark).
+     * If the decoded symbols is not in the allowed character set, an exception must be thrown.
+     * 5) Concatenate the individual characters together to form a message
      */
 
     /**
@@ -45,14 +55,7 @@ public class Encoder {
     /**
      * An encoding function. Takes in a "plaintext message", returns a bit array
      *
-     * @param message A message where each character can only be one of the following:
-     *                - lowercase letters a-z
-     *                - uppercase letters A-Z
-     *                - digits 0-9
-     *                - space ' '
-     *                - comma ','
-     *                - period '.'
-     *                - exclamation mark '.'
+     * @param message A message where each character can only be one of the allowed symbols, see the protocol
      * @return The message encoded as a bit string according to the protocol described above
      * each bit must be represented as one character in a string. For example, if the result is 10101011,
      * return the string "10101011"
@@ -70,12 +73,12 @@ public class Encoder {
      * Inverse function for encode(m). When implemented correctly, decode(encode(m)) == m
      *
      * @param binaryString The signal in a binary-string format, consisting of ones and zeroes, as a string. For example,
-     *                     binary 10101011 will be represented as a string "10101011".
-     *                     Constraints for the input data:
-     *                     - Each character in the binaryString must be either '0' or '1', no other values allowed
-     *                     - The binaryString must always consist of 8-character blocks, where each block will be
-     *                     translated to one character in the decoded message. For example, "01000001" is
-     *                     translated to "A".
+     *        binary 10101011 will be represented as a string "10101011".
+     *        Constraints for the input data:
+     *        - Each character in the binaryString must be either '0' or '1', no other values allowed
+     *        - The binaryString must always consist of 8-character blocks, where each block will be
+     *        translated to one character in the decoded message. For example, "01000001" is
+     *        translated to "A".
      * @return Decoded message, as a string.
      * If the binaryString is null, return null.
      * If the binarySignal is an empty string, return empty string
