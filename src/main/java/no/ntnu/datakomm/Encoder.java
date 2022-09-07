3b6ba8817d1e0a9572fe63fedf1cec733dbb6a8b
Girts Strazdins <girts.strazdins@gmail.com>
2022-09-07 20:08:52 +0200

    Protocol described

--- src/main/java/no/ntnu/datakomm/Encoder.java
+++ src/main/java/no/ntnu/datakomm/Encoder.java
@@ -5,7 +5,28 @@ package no.ntnu.datakomm;
  */
 public class Encoder {
     /*
-     * TODO - describe the protocol
+     * ------------------------------------------------------------
+     * The protocol for encoding
+     * ------------------------------------------------------------
+     * Human-readable message strings are encoded to bit-strings according to the following rules:
+     * 1) Each character of the message is translated to an integer code first, by taking the ASCII code of that
+     *    character. See ASCII table here: https://www.asciitable.com/
+     *    Examples: uppercase letter `A` has ASCII code 65, letter `B` has ASCII code 66, etc.; while lowercase
+     *    letter `a` has code 97, lowercase `b` has code 98, etc.
+     * 2) Then the integer code is translated to a binary string, always eight bits for each integer code.
+     *    The binary string contains the most significant bit first.
+     *    Examples: number 65 is translated to "01000001", 66 is translated to "01000010", 98 is translated to
+     *    "01100010". Number 0 is translated to "00000000" and number 255 is translated to "11111111".
+     * 3) The binary strings for each character are then concatenated in the order as they appear.
+     *    Examples:
+     *      "ABC" -> 65, 66, 67 -> "010000010100001001000011"
+     *      "Hi!" -> 72, 105, 33 -> "010010000110100100100001"
+     *
+     * Conversion in the opposite direction happens according to the same rules, just in the opposite direction:
+     * 1) Split the bit string into 8-character chunks (8-bit blocks)
+     * 2) Convert each 8-bit block to an integer
+     * 3) Convert the integer to a character, using the ASCII table
+     * 4) Concatenate the individual characters together to form a message
      */
 
     /**
@@ -17,6 +38,8 @@ public class Encoder {
         System.out.println("This is the encoding exercise, used in the course IDATA2304!");
         System.out.println("You can run the unit tests by either executing `mvn test` in the terminal, or");
         System.out.println("right click on the src/test/java and choose Run 'All tests'");
+        // You can use this method for some experimentation, if needed - run som encoding and decoding tests
+        // Better yet - create your own unit tests (or run those provided)
     }
 
     /**
@@ -30,7 +53,7 @@ public class Encoder {
      *                - comma ','
      *                - period '.'
      *                - exclamation mark '.'
-     * @return The message encoded as a bit stream according to the protocol described above
+     * @return The message encoded as a bit string according to the protocol described above
      * each bit must be represented as one character in a string. For example, if the result is 10101011,
      * return the string "10101011"
      * If the input message is null or an empty string, return null
@@ -46,18 +69,20 @@ public class Encoder {
      * Read a binary signal (ones and zeroes), decode it to a human-readable message.
      * Inverse function for encode(m). When implemented correctly, decode(encode(m)) == m
      *
-     * @param binarySignal The signal in a binary format, consisting of ones and zeroes, as a string. For example,
+     * @param binaryString The signal in a binary-string format, consisting of ones and zeroes, as a string. For example,
      *                     binary 10101011 will be represented as a string "10101011".
      *                     Constraints for the input data:
-     *                     - Each character in the binarySignal must be either '0' or '1', no other values allowed
-     *                     - The binarySignal must always consist of 8-character blocks, where each block will be
-     *                     translated to one character in the decoded message. For example,
+     *                     - Each character in the binaryString must be either '0' or '1', no other values allowed
+     *                     - The binaryString must always consist of 8-character blocks, where each block will be
+     *                     translated to one character in the decoded message. For example, "01000001" is
+     *                     translated to "A".
      * @return Decoded message, as a string.
-     * If the binarySignal is null, return null.
+     * If the binaryString is null, return null.
+     * If the binarySignal is an empty string, return empty string
      * If the binarySignal is an empty string, return empty string
      * @throws IllegalArgumentException If the format for binarySignal is invalid
      */
-    public static String decode(String binarySignal) throws IllegalArgumentException {
+    public static String decode(String binaryString) throws IllegalArgumentException {
         // TODO - implement this function
         throw new UnsupportedOperationException("Feature not implemented");
     }
